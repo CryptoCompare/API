fb5658b4989cf9ee02ec0aca559e43038fd5e29d
YAGYANK CHADHA <yagyank@gmail.com>
2017-07-27 01:19:07 +0800

    New Version

--- catalog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^liveData/(?P<siteId>[0-9]+)/(?P<currency>[A-Z]+)/$', views.liveData.as_view()),
-    url(r'^zebpaHistory/(?P<currency>[A-Z]+)/$', views.zebpaHistory.as_view()),
-]
