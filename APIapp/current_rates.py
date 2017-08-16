106b15e3e16b2823d24ead218ae32197a3cd3638
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-16 18:56:59 +0800

    Update current_rates.py

--- APIapp/current_rates.py
+++ APIapp/current_rates.py
@@ -1,5 +1,5 @@
 import requests as r
-
+import json
 """
 API will contain 1, 2 or 3 fields in the JSON file
 
@@ -32,16 +32,28 @@ data = {
 	"sell": -1,
 	"volume": -1
 }
+response_data = {
+	"currency": "CUR",
+	"id": -1,
+	"success": False,
+	"buy": -1,
+	"sell": -1,
+	"volume": -1
+
+}
 
 def getRate(apiInfo):
+	data['success'] = False
+	data['buy'] = -1
+	data['sell'] = -1
+	data['volume'] = -1
 	if(len(apiInfo)>0):
 		if 'all' in apiInfo:
 			api = apiInfo['all']
-			if 'volumeKey' in apiInfo:
+			if 'volumeKey' in api:
 				sendRequest(api['endpoint'], api['buyKey'], api['sellKey'], api['volumeKey'])
 			else:
 				sendRequest(api['endpoint'], api['buyKey'], api['sellKey'], -1)
-			print data
 		else :
 			apiBuy = apiInfo['buy']
 			sendRequest(apiBuy['endpoint'], apiBuy['buyKey'], -1, -1)
@@ -52,9 +64,7 @@ def getRate(apiInfo):
 			if 'volume' in apiInfo:
 				apiVolume = apiInfo['volume']
 				sendRequest(apiVolume['endpoint'], -1, -1, apiVolume['volumeKey'])
-			print data
-
-
+		return data
 
 def sendRequest(url, buyKey, sellKey, volumeKey):
 	response = r.get(url)
@@ -88,8 +98,21 @@ def sendRequest(url, buyKey, sellKey, volumeKey):
 			val = val[key]
 		data['volume'] = val
 
-# url = "https://api.coinbase.com/v2/prices/sell?currency=SGD"
-# buyKey = "data.amount"
-# sellKey = "data.currency"
-# volumeKey = "data.currency"
-# sendRequest(url, buyKey, sellKey, volumeKey)
+filename = "exchanges.json"
+
+try:
+	f = open(filename, 'r')
+	json_data = json.loads(f.read())
+	for key, value in json_data.iteritems():
+			for site in value:
+				getRate(site['api'])
+				response_data["currency"] = key
+				response_data["id"] = site['id']
+				response_data["success"] = data["success"]
+				response_data["buy"] = data["buy"]
+				response_data["sell"] = data["sell"]
+				response_data["volume"] = data["volume"]
+				print response_data
+	f.close()
+except IOError:
+	print 'problem reading: ' + filename
