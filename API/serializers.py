e125c6bada69865bfda8645c2318362dfe67835e
yagyankchadha <yagyank@gmail.com>
2017-10-19 21:41:30 +0530

    return currency in live data

--- API/serializers.py
+++ API/serializers.py
@@ -6,7 +6,7 @@ class BitcoinLiveDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = BitcoinLiveData
         fields = ('buy', 'sell', 'lastHourMinBuy', 'lastDayMinBuy', 'lastWeekMinBuy', 'lastMonthMinBuy', 'lastHourMaxBuy', 'lastDayMaxBuy', 'lastWeekMaxBuy', 'lastMonthMaxBuy',
-        	 'lastHourMinSell', 'lastDayMinSell', 'lastWeekMinSell', 'lastMonthMinSell', 'lastHourMaxSell', 'lastDayMaxSell', 'lastWeekMaxSell', 'lastMonthMaxSell')
+        	 'lastHourMinSell', 'lastDayMinSell', 'lastWeekMinSell', 'lastMonthMinSell', 'lastHourMaxSell', 'lastDayMaxSell', 'lastWeekMaxSell', 'lastMonthMaxSell', 'currency')
 
 class BitcoinHistorySerializer(serializers.ModelSerializer):
 	class Meta:
